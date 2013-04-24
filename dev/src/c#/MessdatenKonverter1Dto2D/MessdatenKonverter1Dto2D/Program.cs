3c63d85e912d927b6b6053247bb075829b97fd1d
cmg-dev <christoph.gnip@gmail.com>
2013-04-24 14:48:28 +0200

    Converter for Measurements started

---
+++ dev/src/c#/MessdatenKonverter1Dto2D/MessdatenKonverter1Dto2D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessdatenKonverter1Dto2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
