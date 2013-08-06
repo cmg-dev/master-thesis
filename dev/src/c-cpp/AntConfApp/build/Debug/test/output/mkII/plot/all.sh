2fd1ef300d249c9ae39b39dcd0c5b7f824792c39
Christoph Gnip <christoph.gnip@gmail.com>
2013-08-06 15:15:53 +0200

    All plot commands in one script

---
+++ dev/src/c-cpp/AntConfApp/build/Debug/test/output/mkII/plot/all.sh
@@ -0,0 +1,25 @@
+
+./plot.sh -f E16 -s 0 -e 200 -a 8 -t 1 -p no -c yes && \ 
+./plot.sh -f E17 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E18 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E19 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E20 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E21 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E22 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E23 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E24 -s 0 -e 200 -a 8 -t 1 -p no -c yes && \ 
+./plot.sh -f E25 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E26 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E27 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E28 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E29 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E30 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E31 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E32 -s 0 -e 200 -a 8 -t 1 -p no -c yes && \ 
+./plot.sh -f E34 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E35 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E36 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E37 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E38 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E39 -s 0 -e 200 -a 9 -t 1 -p no -c yes && \
+./plot.sh -f E40 -s 0 -e 200 -a 9 -t 1 -p no -c yes
