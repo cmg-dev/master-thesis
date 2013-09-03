abea219df7345ffec77af536577b413f56ece071
cmg-dev <christoph.gnip@gmail.com>
2013-09-03 16:44:17 +0200

    Thesis store

--- documentation/thesis/src/EvolutionaryCalibration.cpp
+++ documentation/thesis/src/EvolutionaryCalibration.cpp
@@ -1,4 +1,3 @@
-
 #include "EvolutionaryCalibration.h"
 
 namespace PRPSEvolution {
--- documentation/thesis/src/EvolutionaryCalibration.h
+++ documentation/thesis/src/EvolutionaryCalibration.h
@@ -15,97 +15,97 @@ namespace PRPSEvolution {
 		/**
 		*
 		*/
-		struct EvolutionaryCalibration : public SingleObjectiveFunction {
+struct EvolutionaryCalibration : public SingleObjectiveFunction {
 
-			typedef AbstractOptimizer<shark::VectorSpace< double >,double,SingleObjectiveResultSet<typename shark::VectorSpace< double >::PointType> > base_type;
+	typedef AbstractOptimizer<shark::VectorSpace< double >,double,SingleObjectiveResultSet<typename shark::VectorSpace< double >::PointType> > base_type;
 
-			typedef typename base_type::ObjectiveFunctionType ObjectiveFunctionType;
+	typedef typename base_type::ObjectiveFunctionType ObjectiveFunctionType;
 
-			EvolutionaryCalibration( ) {
-				m_numberOfVariables = Solve::ProblemDimensions::Calibration;
-				m_features |= CAN_PROPOSE_STARTING_POINT;
-			}
+	EvolutionaryCalibration( ) {
+		m_numberOfVariables = Solve::ProblemDimensions::Calibration;
+		m_features |= CAN_PROPOSE_STARTING_POINT;
+	}
 
-			/// \brief From INameable: return the class name.
-			std::string name() const
-			{ return "Evolutionary Calibration"; }
+	/// \brief From INameable: return the class name.
+	std::string name() const
+	{ return "Evolutionary Calibration"; }
 
-			std::size_t numberOfVariables()const{
-				return m_numberOfVariables;
-			}
+	std::size_t numberOfVariables()const{
+		return m_numberOfVariables;
+	}
 
-			bool hasScalableDimensionality()const{
-				return true;
-			}
+	bool hasScalableDimensionality()const{
+		return true;
+	}
 
-			void setNumberOfVariables( std::size_t numberOfVariables ){
-				m_numberOfVariables = numberOfVariables;
-			}
+	void setNumberOfVariables( std::size_t numberOfVariables ){
+		m_numberOfVariables = numberOfVariables;
+	}
 
-			void configure(const PropertyTree &node) {
-				m_numberOfVariables = node.get("numberOfVariables", 5l);
-			}
+	void configure(const PropertyTree &node) {
+		m_numberOfVariables = node.get("numberOfVariables", 5l);
+	}
 
-			/**
-			 * Generate a starting value
-			 * @param[out] x The suggested search point
-			 * 
-			 */
-			void proposeStartingPoint(SearchPointType &x) const {
-				x.resize(numberOfVariables());
+	/**
+	 * Generate a starting value
+	 * @param[out] x The suggested search point
+	 * 
+	 */
+	void proposeStartingPoint(SearchPointType &x) const {
+		x.resize(numberOfVariables());
 
-				for (unsigned int i = 0; i < 3; i++) {
-					x( i ) = Rng::uni(-10, 10);
-				}
-			}
+		for (unsigned int i = 0; i < 3; i++) {
+			x( i ) = Rng::uni(-10, 10);
+		}
+	}
 
-			/**
-			 *
-			 */
-			double eval(const SearchPointType &p) const;
+	/**
+	 *
+	 */
+	double eval(const SearchPointType &p) const;
 
-			/**
-			 *
-			 */
-			void setParams( const NRmatrix< Doub > &M,
-							const NRvector< Doub > &v
-						) {
-				setMat(M);
-				setVec(v);
-			}
+	/**
+	 *
+	 */
+	void setParams( const NRmatrix< Doub > &M,
+					const NRvector< Doub > &v
+				) {
+		setMat(M);
+		setVec(v);
+	}
 
 
-			/**
-			*
-			*/
-			void setMat( const NRmatrix< Doub > &M ) {
-				A = M;
-				A_isSet = true;
+	/**
+	*
+	*/
+	void setMat( const NRmatrix< Doub > &M ) {
+		A = M;
+		A_isSet = true;
 
-			}
+	}
 
-			/**
-			*
-			*/
-			void setVec( const NRvector< Doub > &v ) {
-				b = v;
-				b_isSet = true;
-			}
+	/**
+	*
+	*/
+	void setVec( const NRvector< Doub > &v ) {
+		b = v;
+		b_isSet = true;
+	}
 
-			inline double mkII( const NRmatrix<Doub> &A, const double* x, const NRvector<Doub> &b ) const;
+	inline double mkII( const NRmatrix<Doub> &A, const double* x, const NRvector<Doub> &b ) const;
 
-		private:
-			std::size_t m_numberOfVariables;
+private:
+	std::size_t m_numberOfVariables;
 
-			/** The Matrices we need to solve the Problem */
-			NRmatrix< Doub > A;
-			bool A_isSet = false;
+	/** The Matrices we need to solve the Problem */
+	NRmatrix< Doub > A;
+	bool A_isSet = false;
 
-			/** The b-vector needed to find a Solution */
-			NRvector< Doub > b;
-			bool b_isSet = false;
+	/** The b-vector needed to find a Solution */
+	NRvector< Doub > b;
+	bool b_isSet = false;
 
-		};
+};
 
 		ANNOUNCE_SINGLE_OBJECTIVE_FUNCTION(EvolutionaryCalibration, soo::RealValuedObjectiveFunctionFactory);
 
